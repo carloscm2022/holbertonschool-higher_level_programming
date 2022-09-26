922461214439d81856777fcff9f89c0c23f80273
Carlos <carloscr7cmreal@gmail.com>
2022-09-25 18:30:09 -0700

    uploading task 0 finish

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This is the "Square"  module.
+This module provides a simple Square class.
+"""
+
+
 class Square:
-    """Empty class square that defines a Square"""
+    """A simple Square class"""
     pass
