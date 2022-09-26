e1b6adb22bb90bfaa6c98bcbe2060fdc273df6c7
Carlos <carloscr7cmreal@gmail.com>
2022-09-25 18:45:41 -0700

    uploading task 5 finish

---
+++ python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""My first square"""
+
+
+class Square:
+    """Class define a square"""
+    __size = ''
+
+    def __init__(self, size=0):
+        """init"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """Instance that return current square area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Retrieve it"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Use setter"""
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if self.size is 0:
+            print()
+        for i in range(self.size):
+            for j in range(self.size):
+                print('#', end='')
+            print()
