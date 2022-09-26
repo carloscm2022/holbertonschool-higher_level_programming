6461eb482d12b427cf9f462e1e5d23579c162554
Carlos <carloscr7cmreal@gmail.com>
2022-09-25 18:44:50 -0700

    uploading task 3 finish

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""My first square"""
+
+
+class Square:
+    """Class define a square"""
+    __size = ''
+
+    def __init__(self, size=0):
+        """init"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """Instance that return current square area"""
+        return(self.__size * self.__size)
