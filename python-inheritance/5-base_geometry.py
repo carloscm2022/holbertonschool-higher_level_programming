dc5334aeb1a7f2fb5a4f02a31be3b36a0cd2fa4b
Carlos <carloscr7cmreal@gmail.com>
2022-10-02 13:19:45 -0700

    task 5 empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Geometry module"""
+
+
+class BaseGeometry:
+    """Class Base"""
+
+    def __dir__(self):
+        """function __dir__ """
+        obj = BaseGeometry
+        return dir(obj)
