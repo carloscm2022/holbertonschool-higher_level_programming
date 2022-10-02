2655361c1770497bd3885b3a4c31dd144f4b7513
Carlos <carloscr7cmreal@gmail.com>
2022-10-02 13:22:20 -0700

    task 10 class Square that inherits from Rectangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module named 10-square.py
+   Created on Thursday, September 29, 2022
+   @author: Daisy Chipana Lapa
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This class Square
+    Attributes:
+       Nothing
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return super().area()
