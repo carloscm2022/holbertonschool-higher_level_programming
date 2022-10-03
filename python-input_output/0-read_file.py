7309e20bbcafed0ece08f57d601509acc3141305
Carlos <carloscr7cmreal@gmail.com>
2022-10-02 21:39:54 -0700

    task 0 correction

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-# 0-read_file.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a text file-reading function."""
+"""Read file"""
 
 
 def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """
+    function that reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, 'r', encoding='utf8') as file:
+        a = file.read()
+        print(a, end='')
