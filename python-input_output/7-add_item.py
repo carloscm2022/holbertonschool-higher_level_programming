150b8c5fb69d2cb4fa0751803b1a74d5e672a18a
Carlos <carloscr7cmreal@gmail.com>
2022-10-02 18:35:00 -0700

    finish correction task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""module that adds all arguments to a Python list, and then saves
-them to a file"""
-from sys import argv
+# 9-add_item.py
+# Carlos Barros <1543@holbertonschool.com>
+""" File name : 9-add_item.py"""
+import sys
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-try:
-    a = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    a = []
-b = list(argv)
-b.pop(0)
-a.extend(b)
-
-save_to_json_file(a, "add_item.json")
+    try:
+        items = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, 'add_item.json')
