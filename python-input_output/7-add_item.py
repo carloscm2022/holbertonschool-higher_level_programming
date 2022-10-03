9e743776679e28cf0866260297ed5eb5729fbcfa
Carlos <carloscr7cmreal@gmail.com>
2022-10-02 18:11:03 -0700

    task 7 Fix head

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-# 9-add_item.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+"""
+Module 7-add_item
+Contains function that adds and saves to Python obj to JSON file; loads objects
+"""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    existing_content = load_from_json_file(filename)
+except FileNotFoundError:
+    existing_content = []
+
+save_to_json_file(existing_content + argv[1:], filename)
