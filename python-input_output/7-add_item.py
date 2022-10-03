52161895276768bdec2046263bd96d7dad3ec2f0
Carlos <carloscr7cmreal@gmail.com>
2022-10-02 21:59:59 -0700

    task 7 finish

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""Load, add save"""
+"""
+Module 7-add_item.py
+Contains function that adds and saves to Python obj to JSON file; loads objects
+"""
+
+
 import json
 import sys
 import os
 
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = 'add_item.json'
-with open(filename, 'a+') as f:
-    pass
-
-try:
-    newlist = load_from_json_file(filename)
-except ValueError:
-    newlist = []
-l = sys.argv[1:]
-newlist += l
-save_to_json_file(newlist, filename)
+len_arg = len(sys.argv)
+filename = "add_item.json"
+if not os.path.exists(filename):
+    my_list = []
+else:
+    my_list = load_from_json_file(filename)
+for i in range(1, len_arg):
+    my_list.append(sys.argv[i])
+save_to_json_file(my_list, filename)
